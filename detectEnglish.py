f7ecbb527850139e3839e6a3dc01590528962e73
Eric Robinson <eric.allen.robinson@gmail.com>
2024-07-28 22:14:50 -0400

    Changes to be committed:     added words.txt dictionary and     updated detectEnglish to use the new dictionary 	modified:   detectEnglish.py     new file:   words.txt

--- detectEnglish.py
+++ detectEnglish.py
@@ -12,7 +12,7 @@ UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LETTERS_AND_SPACE = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
 
 def loadDictionary():
-    dictionaryFile = open('dictionary.txt')
+    dictionaryFile = open('words.txt')
     englishWords = {}
     for word in dictionaryFile.read().split('\n'):
         englishWords[word] = None
